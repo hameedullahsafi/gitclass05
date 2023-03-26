0c44b9e112bf301b296a1c1d6c18b664a283a736
hameedullahsafi <omar.linguist.mil@gmail.com>
2023-03-26 14:02:13 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
-        for(int i=0;i<10;i++){
+        for(int i=0;i<5;i++){
             System.out.println(i);
         }
+/*
+**Git class05*
+Cloning a project
 
+Approach:
+
+1.create a folder on desktop
+2.navigate ur terminal there
+3.get the url from the github  -- > in code --> under https://
+4.git clone url
+---
+if u want to open the repo u cloned in the gitbash
+
+5.u need to naviagte to it
+
+
+
+---------------------------------------------------
+
+Message #🎙live-questions
+
+ */
     }
 }
